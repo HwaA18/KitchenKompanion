93615e4780f092e7e68397374457cde4aeeea01d
oakakpoa <92894059+oakakpoa@users.noreply.github.com>
2021-10-27 11:44:19 -0400

    Wed_10/27

--- app/src/main/java/com/example/kitchenkompanion/MainActivity2.java
+++ app/src/main/java/com/example/kitchenkompanion/MainActivity2.java
@@ -4,16 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    EditText editText;
+    Button addButton;
+    ArrayList<String> listItems;
+    ArrayAdapter<String> adapter;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        getSupportActionBar().setTitle("here we are");
+        getSupportActionBar().setTitle("Shopping List");
+
+        editText = (EditText) findViewById(R.id.editTextItemName);
+        addButton = (Button) findViewById(R.id.button2);
+
+        listItems = new ArrayList<String>();
+        listItems.add("First Item - added on Activity Create");
+        adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, listItems);
+        listView.setAdapter(adapter);
     }
 
     public void onAddItem(View view) {
     }
+
+
 }
