e53986ceb2536ebc63a0a11f5ef900c7898eb112
Kevin Tat <kevin6789101112@LAPTOP-E4T8QAPB.localdomain>
2021-10-28 16:16:10 -0400

    added inFridge variable

--- app/src/main/java/com/example/kitchenkompanion/Item.java
+++ app/src/main/java/com/example/kitchenkompanion/Item.java
@@ -4,14 +4,17 @@ public class Item {
     String name;
     String img;
     int quantity;
+    boolean inFridge;
 
-    public Item (String name, String img, int quantity) {
+    public Item (String name, String img, int quantity, boolean inFridge) {
         this.name = name;
         this.img = img;
         this.quantity = quantity;
+        this.inFridge = inFridge;
     }
 
     public String getName() {return this.name;}
     public String getImg() {return this.img;}
     public int getQuantity() {return this.quantity;}
+    public boolean isInFridge() {return inFridge; }
 }
