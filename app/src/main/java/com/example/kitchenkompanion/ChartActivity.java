e9194140d5638375cd7885cfd21102ce7546941c
oakakpoa <92894059+oakakpoa@users.noreply.github.com>
2021-11-07 12:23:30 -0500

    AddedEditRemoveWritetoFile

--- app/src/main/java/com/example/kitchenkompanion/ChartActivity.java
+++ app/src/main/java/com/example/kitchenkompanion/ChartActivity.java
@@ -13,30 +13,36 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import org.apache.commons.io.FileUtils;
-
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Scanner;
 
 
 public class ChartActivity extends Activity {
 
     ListView liv;
     Button btn ;
+    Button end ;
     TextView name;
     TextView price;
+    TextView expDate;
     TextView qty;
     ArrayList<Item> items;
     ArrayAdapter<Item> adp ;
     ItemAdapter itemAdp;
-    String nameStr;
+    String nameStr, expStr;
     String info = "";
     int qtySTr;
     double priceStr;
-    File filesDir;
-    File todoFile;
+    File filesDir, updated;
+    FileOutputStream fileOut, fileOutUp;
+    PrintWriter writer, writerUp;
+
 
 
 
@@ -54,34 +60,80 @@ public class ChartActivity extends Activity {
         name = (TextView) findViewById(R.id.editTextTextPersonName3);
         price = (TextView) findViewById(R.id.editTextTextPersonName);
         qty = (TextView) findViewById(R.id.editTextTextPersonName2);
+        end = (Button) findViewById(R.id.finish);
+        expDate = (TextView) findViewById(R.id.exp) ;
         items = new ArrayList<>();
         itemAdp = new ItemAdapter(this,items);
+
         adp = new ArrayAdapter<Item>(this,
                 android.R.layout.simple_spinner_dropdown_item, items);
-        filesDir = new File(getApplicationContext().getFilesDir(), "content.txt");
+        filesDir = new File(ChartActivity.this.getFilesDir(), "inventory.txt");
+        updated = new File(ChartActivity.this.getFilesDir(), "uptodate.txt");
+        try {
+            fileOut = new FileOutputStream(filesDir, true);
+            writer = new PrintWriter(fileOut);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
 
         try {
+            //File myObj = new File("inventory.txt");
+            Scanner myReader = new Scanner(filesDir);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                String[] split = data.split("\\s+");
 
-            FileUtils.writeStringToFile(filesDir, "Item Name\t" +
-                    "Price\t\tQuantity");
-        } catch (IOException e) {
+                Item dummy = new Item(split[0],Double.parseDouble(split[1]),
+                        Integer.parseInt(split[2]), split[3]);
+                add(dummy);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
             e.printStackTrace();
         }
-        Item t = new Item("bate", 50, 3);
 
-        itemAdp.addItem(t);
+
+       /* try {
+            Date date = new Date();
+            SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yy");
+            String str = formatter.format(date);
+            writer.println("Item Name\t" +
+                    "Price\tQuantity\tExp.Date\n");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
+
+       Item r = new Item("jello", 30, 2, "02/22");
+       items.add(r);
+
+        /*for(int i = 0; i< 5; i++)
+        {
+            items.add(r);
+            itemAdp.addItem(r);
+        }*/
+
+
+
         setupListViewListener();
 
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(name.getText().toString() == null || name.getText().toString().length() == 0)
+                    return;
 
-                if(!name.getText().toString().isEmpty() && !price.getText().toString().isEmpty()
-                        && !qty.getText().toString().isEmpty())
+                /*if(!name.getText().toString().isEmpty() && !price.getText().toString().isEmpty()
+                        && !qty.getText().toString().isEmpty() && !expDate.getText().toString().isEmpty())
+                {
+
+                }*/
+                else
                 {
                     nameStr = name.getText().toString();
+                    expStr = expDate.getText().toString();
                     try
                     {
                         qtySTr = Integer.parseInt(qty.getText().toString());
@@ -100,23 +152,74 @@ public class ChartActivity extends Activity {
                         priceStr = 0;
                     }
 
-                    Item item = new Item(nameStr, priceStr, qtySTr);
-                    info = ""+ nameStr+"\t"+priceStr+"\t\t"+qtySTr+"\n";
+                    Item item = new Item(nameStr, priceStr, qtySTr, expStr);
+                    String p, q, ex;
+                    nameStr = leftpad(nameStr, 15);
+                    p = ""+priceStr;
+                    p = leftpad(p, 15);
+                    q = ""+qtySTr;
+                    q = leftpad(q, 15);
+
+                    ex = ""+expStr;
+                    ex = leftpad(ex, 15);
+
+
+                    info = nameStr+p+q+ex;
+                            //""+ item.getName()+"\t"+item.getPrice()+"\t"+item.getQuantity()
+                            //+"\t"+item.getExp();
+                    try {
+                        writer.println(info);
+
+                        //FileUtils.writeStringToFile(filesDir, info);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
 
                     add(item);
+
                     name.setText("");
                     qty.setText("");
                     price.setText("");
-                    try {
+                    expDate.setText("");
 
-                        FileUtils.writeStringToFile(filesDir, info);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+
 
 
                 }
 
+            }
+        });
+
+        end.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                try{
+                    writer.flush();
+                    writer.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                try {
+                    String  nameUp, priceUp, qtyUp, expUp;
+                    fileOutUp = new FileOutputStream(updated);
+                    writerUp = new PrintWriter(fileOutUp);
+                    for(int i = 0; i<items.size();i++)
+                    {
+                        nameUp = leftpad(items.get(i).getName(), 15);
+                        priceUp = leftpad(""+items.get(i).getPrice(), 15);
+                        qtyUp = leftpad(""+items.get(i).getQuantity(), 15);
+                        expUp = leftpad(items.get(i).getExp(), 15);
+                        writerUp.println(nameUp+priceUp+qtyUp+expUp);
+                    }
+                    writerUp.flush();
+                    writerUp.close();
+
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+                finish();
             }
         });
     }
@@ -145,9 +248,16 @@ public class ChartActivity extends Activity {
                                 adp.notifyDataSetChanged();
                                 // Return true consumes the long click event (marks it handled)
 
+
                             }
                         })
                         .setNegativeButton("Cancel",null)
+                        .setNeutralButton("Edit", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                //Under construction
+                            }
+                        })
                         .show();
 
                 return true;
@@ -162,18 +272,18 @@ public class ChartActivity extends Activity {
     public void add(Item ne) {
 
         itemAdp.addItem(ne);
-
         liv.setAdapter(itemAdp);
         adp.notifyDataSetChanged();
 
     }
 
-
+    private String leftpad(String text, int length) {
+        return String.format("%-" + length + "." + length + "s", text);
+    }
     private void writeItems() {
 
 
     }
-
 
 
 
--- app/src/main/java/com/example/kitchenkompanion/Item.java
+++ app/src/main/java/com/example/kitchenkompanion/Item.java
@@ -1,26 +1,29 @@
 package com.example.kitchenkompanion;
 
 public class Item {
-    String name;
+    String name, exp;
     int quantity;
     double price;
     boolean inFridge;
 
-    public Item (String name, double price, int quantity, boolean inFridge) {
+    public Item (String name, double price, int quantity, boolean inFridge, String exp) {
         this.name = name;
         //this.img = img;
         this.price = price;
         this.quantity = quantity;
         this.inFridge = inFridge;
+        this.exp = exp;
     }
-    public Item (String name, double price, int quantity) {
+    public Item (String name, double price, int quantity, String exp) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
         this.inFridge = false;
+        this.exp = exp;
     }
 
     public String getName() {return this.name;}
+    public String getExp() {return this.exp;}
     public double getPrice() {return this.price;}
     public int getQuantity() {return this.quantity;}
     public boolean isInFridge() {return inFridge; }
